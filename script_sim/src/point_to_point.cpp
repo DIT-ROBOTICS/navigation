a79167558303272783896a9bdcc112f018d5444d
Drychang <jimmy.chang2003@gmail.com>
2023-02-19 18:27:14 +0800

    script_sim push

--- script_sim/src/point_to_point.cpp
+++ script_sim/src/point_to_point.cpp
@@ -3,7 +3,7 @@
 #include "yaml-cpp/yaml.h"
 #include "std_msgs/Float64.h"
 #include <iostream>
-
+using namespace std;
 double check = 1;
 
 
@@ -21,27 +21,23 @@ int main(int argc, char **argv)
     ros::Publisher pub = nh.advertise<geometry_msgs::Point>("goal_topic", 10);
     ros::Subscriber sub = nh.subscribe("check_topic",10,Check);
     ros::Rate loop_rate(1);
-    
+
     geometry_msgs::Point goal_point;
     YAML::Node pathConfig = YAML::LoadFile("/home/drychang/winter_ws/src/navigation/script_sim/config/script_sim.yaml");
     // YAML::Node pathConfig = YAML::LoadFile("-d $(find script_sim)/config/script_sim.yaml");
-    while(ros::ok())
-    {
-        // if(action==1){
+    
+        // if(check==1){
             for(auto goal : pathConfig){
                 goal_point.x = goal["xyz"][0].as<double>();
                 goal_point.y = goal["xyz"][1].as<double>();
                 goal_point.z = goal["xyz"][2].as<double>();
                 pub.publish(goal_point);
-                // action=0;
-                // cout <<"x"<<goal["xyz"].as<double>()<< end1;
-                // cout<<"x"<<goal["xyz"].as<double>()<<end1;
-                // cout<<"x"<<goal["xyz"].as<double>()<<end1;
+                // check=0;
+                cout <<"x"<<goal["xyz"][0].as<double>()<< endl;
+                cout<<"y"<<goal["xyz"][1].as<double>()<<endl;
+                cout<<"z"<<goal["xyz"][2].as<double>()<<endl;
             }
         // }
-        ros::spinOnce();
-       loop_rate.sleep();
         
-    }
     return 0;
 }
