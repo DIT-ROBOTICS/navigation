0ebe54249580e56de860f79bc5b507eaaf4e0542
pithreeone <pithreeone@gmail.com>
2023-04-06 23:37:11 +0800

    feat: add new map and add script

---
+++ env_setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install libarmadillo-dev -y
+sudo apt-get install libbullet-dev -y
+sudo apt-get install libsdl1.2-dev -y
+sudo apt-get install libsdl-image1.2-dev -y
+sudo apt-get install qtbase5-dev -y
+sudo apt-get install qtdeclarative5-dev -y
+sudo apt-get install ros-noetic-move-base-msgs -y
+sudo apt-get install ros-noetic-tf2-sensor-msgs -y
