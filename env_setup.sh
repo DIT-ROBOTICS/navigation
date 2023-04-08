b7669daea773449fa0fce4a1908af730f9053be3
YuZhong-Chen <0980027@gmail.com>
2023-04-08 20:36:24 +0800

    feat: update env_setup script.

--- env_setup.sh
+++ env_setup.sh
@@ -1,8 +1,10 @@
-sudo apt-get install libarmadillo-dev -y
-sudo apt-get install libbullet-dev -y
-sudo apt-get install libsdl1.2-dev -y
-sudo apt-get install libsdl-image1.2-dev -y
-sudo apt-get install qtbase5-dev -y
-sudo apt-get install qtdeclarative5-dev -y
-sudo apt-get install ros-noetic-move-base-msgs -y
-sudo apt-get install ros-noetic-tf2-sensor-msgs -y
+set -e
+
+sudo apt install -y libarmadillo-dev \
+                    libbullet-dev \
+                    libsdl1.2-dev \
+                    libsdl-image1.2-dev \
+                    qtbase5-dev \
+                    qtdeclarative5-dev \
+                    ros-noetic-move-base-msgs \
+                    ros-noetic-tf2-sensor-msgs 
