3a78ae52ba526bbf52ca104071f35f647d7e8e63
weic58 <chenwei.c0508@gmail.com>
2023-05-03 21:08:24 +0800

    ADD: instructions for rewriting ch.sh

--- ch.sh
+++ ch.sh
@@ -1,25 +1,42 @@
 #!/bin/bash -i
 
+#$1 means first argument in command [. ch.sh] [IP]
+#                                      $0      $1
 hostname=$1
 
+#Assign IP to hostname
 case $1 in
     "p1" )
         hostname="192.168.50.118"
-        echo "Change ROS Master to 118!"
+        echo "Change ROS Master to ${hostname}!"
     ;;
     "p2" )
         hostname="192.168.50.174"
-        echo "Change ROS Master to 174!"
+        echo "Change ROS Master to ${hostname}!"
     ;;
+    "o1" )
+        hostname="192.168.50.210"
+        echo "Change ROS Master to ${hostname}!"
+    ;;
+    "o2" )
+        hostname="192.168.50.238"
+        echo "Change ROS Master to ${hostname}!"
+    ;;
+    # "alias" )
+    #     hostname="192.168.50.xxx"
+    #     echo "Change ROS Master to ${hostname}!"
+    # ;;
     "192.168.50."* )
         echo "Change ROS Master to ${hostname}!"
     ;;
     * )
-        echo "IP Format may be wrong! Try again!"
+        echo "IP Format may be wrong: ${hostname}! Try again!"
         exit
     ;;
 esac
 
+# Change .bashrc but skip #commented lines
 sed -i "/^[^#]/s%ROS_MASTER_URI=.*%ROS_MASTER_URI=http://${hostname}:11311%g" ~/.bashrc
 
+# Source .bashrc(At the first line, you need to add "-i", which means open "interactive mode".)
 source ~/.bashrc
