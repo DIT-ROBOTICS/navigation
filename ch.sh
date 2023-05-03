928b6cdd823c3d4f91dae89349ea0d5a5b85b908
weic58 <chenwei.c0508@gmail.com>
2023-05-03 19:46:55 +0800

    ADD: fast change master script!

---
+++ ch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -i
+
+hostname=$1
+
+case $1 in
+    "p1" )
+        hostname="192.168.50.118"
+        echo "Change ROS Master to 118!"
+    ;;
+    "p2" )
+        hostname="192.168.50.174"
+        echo "Change ROS Master to 174!"
+    ;;
+    "192.168.50.*" )
+        echo "Change ROS Master to ${hostname}!"
+    ;;
+    * )
+        echo "IP Format may be wrong! Try again!"
+        exit
+    ;;
+esac
+
+sed -i "s#ROS_MASTER_URI=.*#ROS_MASTER_URI=http://${hostname}:11311#g" ~/.bashrc
+
+source ~/.bashrc
