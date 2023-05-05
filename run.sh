4e13bda67ea0c64b75434f35a8299dbe77cdef7b
pithreeone <pithreeone@gmail.com>
2023-05-05 19:18:09 +0800

    Perf: tune parameters

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gnome-terminal --working-directory=/home/pithreeone/catkin_ws/src/navigation -- "./robot1_run.sh hub" &
+gnome-terminal --working-directory=/home/pithreeone/catkin_ws/src/navigation -- "./robot1_run.sh run"
