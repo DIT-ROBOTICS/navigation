0d5db5d514e744ff0dfde508eabafc22f59a1329
pithreeone <pithreeone@gmail.com>
2023-05-09 02:38:24 +0800

    fix: cannot multiple launch in shell-script, remove expect

--- navigation_run/script/basic.sh
+++ navigation_run/script/basic.sh
@@ -15,117 +15,127 @@ set cmd_prompt "]#|~]?"
 
 #Set timeout, unit: seconds
 set timeout 3
-
+set delay 0
 if {$which == "hub1"} {
   spawn ssh $user@$host_1
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch navigation_run hub1.launch\r"
-        send "exit \r"
-      }
-    }
+  send "roslaunch navigation_run hub1.launch\r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       # send "roslaunch navigation_run hub1.launch\r"
+  #       send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "run1b"} {
   spawn ssh $user@$host_1
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch navigation_run run_robot1.launch _side:=b\r"
-        send "exit \r"
-      }
-    }
+  send "roslaunch navigation_run run_robot1.launch _side:=b\r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       # send "roslaunch navigation_run run_robot1.launch _side:=b\r"
+  #       send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "run1g"} {
   spawn ssh $user@$host_1
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch navigation_run run_robot1.launch _side:=g\r"
-        send "exit \r"
-      }
-    }
+  send "roslaunch navigation_run run_robot1.launch _side:=g\r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       # send "roslaunch navigation_run run_robot1.launch _side:=g\r"
+  #       send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "hub2"} {
   spawn ssh $user@$host_2
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re "$ " {
-        send "roslaunch navigation_run hub2.launch\r"
-        send "exit \r"
-      }
-    }
+  send "roslaunch navigation_run hub2.launch\r"
+  # expect {
+  #     -re "$ " {
+  #       # send "roslaunch navigation_run hub2.launch\r"
+  #       send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "run2b"} {
   spawn ssh $user@$host_2
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch navigation_run run_robot2.launch _side:=b\r"
-        send "exit \r"
-      }
-    }
+  send "roslaunch navigation_run run_robot2.launch _side:=b\r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       send "roslaunch navigation_run run_robot2.launch _side:=b\r"
+  #       send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "run2g"} {
   spawn ssh $user@$host_2
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch navigation_run run_robot2.launch _side:=g\r"
-        send "exit \r"
-      }
-    }
+  send "roslaunch navigation_run run_robot2.launch _side:=g\r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       send "roslaunch navigation_run run_robot2.launch _side:=g\r"
+  #       send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "cam_a"} {
   spawn ssh $user@$host_cam_a
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch aruco_ros cam_a.launch \r"
-        # send "exit \r"
-      }
-    }
+  send "roslaunch aruco_ros cam_a.launch \r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       send "roslaunch aruco_ros cam_a.launch \r"
+  #       # send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "cam_b"} {
   spawn ssh $user@$host_cam_b
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch aruco_ros cam_b.launch \r"
-        # send "exit \r"
-      }
-    }
+  # send "roslaunch aruco_ros cam_b.launch \r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       send "roslaunch aruco_ros cam_b.launch \r"
+  #       # send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "cam_c"} {
   spawn ssh $user@$host_cam_c
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch mklc side.launch \r"
-        # send "exit \r"
-      }
-    }
+  send "roslaunch mklc side.launch \r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       send "roslaunch mklc side.launch \r"
+  #       # send "exit \r"
+  #     }
+  #   }
 } elseif {$which == "cam_central"} {
   spawn ssh $user_central@$host_cam_central
-  sleep 2
+  sleep $delay
   expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?" {send "yes\r"}
   expect "password:" {send "$loginpass\r"}
-  expect {
-      -re $cmd_prompt {
-        send "roslaunch mklc double.launch\r"
-        # send "exit \r"
-      }
-    }
+  send "roslaunch mklc double.launch\r"
+  # expect {
+  #     -re $cmd_prompt {
+  #       send "roslaunch mklc double.launch\r"
+  #       # send "exit \r"
+  #     }
+  #   }
 } else {
   echo "Something went wrong!"
   exit 1
--- navigation_run/script/run_all.sh
+++ navigation_run/script/run_all.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/bash
-dir="$HOME/navi_ws/src/navigation/navigation_run/script"
-
+dir="$HOME/catkin_ws/src/navigation/navigation_run/script"
+echo ${0}
 # 2 Pandas (Robots)
-if [[ $1="b" ]]
-then
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh run1b'
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub1'
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh run2b'
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub2'
-elif [[ $1="g" ]]
-then
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh run1g'
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub1'
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh run2g'
-    gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub2'
-else
-    echo "Please specify b/g side!"
-fi
+# if [[ $1 = "b" ]]
+# then
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh run1b'
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub1'
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh run2b'
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub2'
+# elif [[ $1 = "g" ]]
+# then
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh run1g'
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub1'
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh run2g'
+#     gnome-terminal --working-directory=$dir -- bash -c './basic.sh hub2'
+# else
+#     echo "Please specify b/g side!"
+# fi
 
-# 3 PIs + 1 Panda (Side)
+# # 3 PIs + 1 Panda (Side)
 gnome-terminal --working-directory=$dir -- bash -c './basic.sh cam_a'
 gnome-terminal --working-directory=$dir -- bash -c './basic.sh cam_b'
 gnome-terminal --working-directory=$dir -- bash -c './basic.sh cam_c'
